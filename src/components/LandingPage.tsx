d8e42ef739cbedf85607ee069c02f81cdb1afdd6
alexk1923 <alexandrukullman@gmail.com>
2022-10-25 20:25:00 +0300

    a

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import "aos/dist/aos.css";
 import player from "../img/player.png";
 import Typewriter from "typewriter-effect";
